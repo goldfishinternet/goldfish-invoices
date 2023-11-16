05710dc3ace02180824f22d3c7d22d0d31171097
Andy Carroll <andy@goldfishinteractive.co.nz>
2023-11-16 08:17:13 +0000

    Add dependency reference

--- app/Models/InvoiceItem.php
+++ app/Models/InvoiceItem.php
@@ -7,6 +7,7 @@ use App\Support\HasAdvancedFilter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class InvoiceItem extends Model
 {
