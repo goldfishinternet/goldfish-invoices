473c130f8aaba8ae971825757f24c749a48c810a
Andy Carroll <andy@goldfishinteractive.co.nz>
2023-11-16 08:12:27 +0000

    Adjust error attributes to fit with bootstraps validation

--- resources/views/livewire/client-contact/form.blade.php
+++ resources/views/livewire/client-contact/form.blade.php
@@ -1,8 +1,8 @@
 <form wire:submit.prevent="submit" class="pt-3">
 
-    <div class="mb-3 {{ $errors->has('clientContact.client_id') ? 'invalid' : '' }}">
+    <div class="mb-3">
         <label class="form-label required" for="client">{{ trans('cruds.clientContact.fields.client') }}</label>
-        <x-select-list class="form-control" required id="client_id" name="client_id" :options="$this->listsForFields['client']" wire:model="clientContact.client_id" />
+        <x-select-list class="form-control {{ $errors->has('clientContact.client_id') ? 'is-invalid' : '' }}" required id="client_id" name="client_id" :options="$this->listsForFields['client']" wire:model="clientContact.client_id" />
         <div class="invalid-feedback">
             {{ $errors->first('clientContact.client_id') }}
         </div>
@@ -10,9 +10,9 @@
             {{ trans('cruds.clientContact.fields.client_helper') }}
         </div>
     </div>
-    <div class="mb-3 {{ $errors->has('clientContact.first_name') ? 'invalid' : '' }}">
+    <div class="mb-3">
         <label class="form-label" for="first_name">{{ trans('cruds.clientContact.fields.first_name') }}</label>
-        <input class="form-control" type="text" name="first_name" id="first_name" wire:model.defer="clientContact.first_name">
+        <input class="form-control {{ $errors->has('clientContact.first_name') ? 'is-invalid' : '' }}" type="text" name="first_name" id="first_name" wire:model.defer="clientContact.first_name">
         <div class="invalid-feedback">
             {{ $errors->first('clientContact.first_name') }}
         </div>
@@ -20,9 +20,9 @@
             {{ trans('cruds.clientContact.fields.first_name_helper') }}
         </div>
     </div>
-    <div class="mb-3 {{ $errors->has('clientContact.last_name') ? 'invalid' : '' }}">
+    <div class="mb-3">
         <label class="form-label" for="last_name">{{ trans('cruds.clientContact.fields.last_name') }}</label>
-        <input class="form-control" type="text" name="last_name" id="last_name" wire:model.defer="clientContact.last_name">
+        <input class="form-control {{ $errors->has('clientContact.last_name') ? 'is-invalid' : '' }}" type="text" name="last_name" id="last_name" wire:model.defer="clientContact.last_name">
         <div class="invalid-feedback">
             {{ $errors->first('clientContact.last_name') }}
         </div>
@@ -30,9 +30,9 @@
             {{ trans('cruds.clientContact.fields.last_name_helper') }}
         </div>
     </div>
-    <div class="mb-3 {{ $errors->has('clientContact.email') ? 'invalid' : '' }}">
+    <div class="mb-3">
         <label class="form-label" for="email">{{ trans('cruds.clientContact.fields.email') }}</label>
-        <input class="form-control" type="text" name="email" id="email" wire:model.defer="clientContact.email">
+        <input class="form-control {{ $errors->has('clientContact.email') ? 'is-invalid' : '' }}" type="text" name="email" id="email" wire:model.defer="clientContact.email">
         <div class="invalid-feedback">
             {{ $errors->first('clientContact.email') }}
         </div>
@@ -40,9 +40,9 @@
             {{ trans('cruds.clientContact.fields.email_helper') }}
         </div>
     </div>
-    <div class="mb-3 {{ $errors->has('clientContact.phone') ? 'invalid' : '' }}">
+    <div class="mb-3">
         <label class="form-label" for="phone">{{ trans('cruds.clientContact.fields.phone') }}</label>
-        <input class="form-control" type="text" name="phone" id="phone_1" wire:model.defer="clientContact.phone">
+        <input class="form-control {{ $errors->has('clientContact.phone') ? 'is-invalid' : '' }}" type="text" name="phone" id="phone_1" wire:model.defer="clientContact.phone">
         <div class="invalid-feedback">
             {{ $errors->first('clientContact.phone') }}
         </div>
@@ -50,9 +50,9 @@
             {{ trans('cruds.clientContact.fields.phone_helper') }}
         </div>
     </div>
-    <div class="mb-3 {{ $errors->has('clientContact.mobile') ? 'invalid' : '' }}">
+    <div class="mb-3">
         <label class="form-label" for="mobile">{{ trans('cruds.clientContact.fields.mobile') }}</label>
-        <input class="form-control" type="text" name="mobile" id="mobile" wire:model.defer="clientContact.mobile">
+        <input class="form-control {{ $errors->has('clientContact.mobile') ? 'is-invalid' : '' }}" type="text" name="mobile" id="mobile" wire:model.defer="clientContact.mobile">
         <div class="invalid-feedback">
             {{ $errors->first('clientContact.mobile') }}
         </div>
@@ -60,7 +60,7 @@
             {{ trans('cruds.clientContact.fields.mobile_helper') }}
         </div>
     </div>
-    <div class="form-group">
+    <div class="mb-3">
         <button class="btn btn-primary mx-1" type="submit">
             {{ trans('global.save') }}
         </button>
